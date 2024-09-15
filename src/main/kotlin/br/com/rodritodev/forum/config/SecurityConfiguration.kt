37d8e3aa2476a675737ff0ac20b964774833ec91
Rodrigo Brito do Nascimento <rodrito90@gmail.com>
2024-09-15 14:57:32 -0300

    Adicionado autenticação com token JWT
    - Implementada geração de token JWT
    - Implementada validação de token
    - Implementado filtro de para autenticação nas request
    - Implementado filtro de para geração de token

--- src/main/kotlin/br/com/rodritodev/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/rodritodev/forum/config/SecurityConfiguration.kt
@@ -1,5 +1,8 @@
 package br.com.rodritodev.forum.config
 
+import br.com.rodritodev.forum.model.Roles
+import br.com.rodritodev.forum.security.JWTAuthenticationFilter
+import br.com.rodritodev.forum.security.JWTLoginFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
@@ -13,6 +16,8 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.filter.OncePerRequestFilter
 
 
 private const val s = "ROLE_USER"
@@ -20,24 +25,29 @@ private const val s = "ROLE_USER"
 @Configuration
 @EnableWebSecurity
 class SecurityConfiguration(
-    private val userDetailService: UserDetailsService
+    private val userDetailService: UserDetailsService,
+    private val jwtUtil: JWTUtil
 ) {
     /**
-     * Essa é a nova abordagem para configurar a segurança sem a necessidade de estender WebSecurityConfigurerAdapter.
-     * Declaramos a segurança diretamente no bean.
+     * Configura o filtro de segurança da aplicação
      *
-     * @param http Configuração de segurança HTTP
-     *
-     * @return Retorna a configuração de segurança
+     * @param http Configuração de segurança
+     * @param authConfig Configuração de autenticação
+     * @return Retorna o filtro de segurança da aplicação
      */
     @Bean
-    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http.csrf { csrf -> csrf.disable() } // Desabilita o CSRF (Cross-Site Request Forgery) para APIs REST
+    fun securityFilterChain(http: HttpSecurity, authConfig: AuthenticationConfiguration): SecurityFilterChain {
+        http
+            .csrf { csrf -> csrf.disable() } // Desabilita o CSRF (Cross-Site Request Forgery) para APIs REST
             .headers { headers -> headers.frameOptions { it.sameOrigin() } } // Permite o uso do console do H2 no mesmo domínio da aplicação
+            .addFilterBefore(JWTLoginFilter(authManager = authenticationManager(authConfig), jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass) // Adiciona os filtros de autenticação e autorização
+            .addFilterBefore(JWTAuthenticationFilter(jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass) // Adiciona o filtro de autenticação JWT antes de qualquer outro filtro de requisição
             .authorizeHttpRequests { authz ->
                 // Configura as autorizações de acesso aos endpoints da aplicação
-                authz.requestMatchers(HttpMethod.POST, "/usuarios")
-                    .hasAuthority("ROLE_ADMIN") // Apenas ADMIN pode cadastrar usuários
+                authz.requestMatchers(HttpMethod.POST, "/login").permitAll() // Permite acesso ao endpoint de login sem autenticação
+                authz.requestMatchers(HttpMethod.POST, "/usuarios/**").hasAuthority(Roles.ROLE_ADMIN.name) // Apenas ADMIN pode cadastrar usuários
+                authz.requestMatchers(HttpMethod.DELETE, "/usuarios/**").hasAuthority(Roles.ROLE_ADMIN.name) // Apenas ADMIN pode excluir usuários
+                authz.requestMatchers("/topicos/**").hasAuthority(Roles.ROLE_USER.name) // Apenas USUÁRIO pode acessar os tópicos
                 authz.requestMatchers("/h2-console/**").permitAll() // Permite acesso ao console do H2 sem autenticação
                 authz.anyRequest().authenticated() // Qualquer outra requisição precisa de autenticação
             }
@@ -45,11 +55,6 @@ class SecurityConfiguration(
                 // Desabilita a criação de sessão no servidor
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             }
-            .httpBasic(Customizer.withDefaults()) // Usar autenticação HTTP Basic
-            .formLogin {
-                it.disable() // Desabilita o formulário de login (não necessário para APIs).
-            }
-
         return http.build()
     }
 
@@ -69,12 +74,20 @@ class SecurityConfiguration(
     }
 
     /**
-     * Configura a autenticação, incluindo o serviço de usuários e o encoder de senha
+     * Configura o `AuthenticationManagerBuilder` para gerenciar a autenticação
+     *
+     * @param auth Gerenciador de autenticação
+     * @throws Exception
      */
     fun configureGlobal(auth: AuthenticationManagerBuilder) {
-        auth.userDetailsService(userDetailService).passwordEncoder(BCryptPasswordEncoder())
+        auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoder())
     }
 
+    /**
+     * Configura o `BCryptPasswordEncoder` para criptografar a senha do usuário
+     *
+     * @return Retorna o codificador de senha
+     */
     @Bean
     fun passwordEncoder(): BCryptPasswordEncoder {
         return BCryptPasswordEncoder()
---
+++ src/main/kotlin/br/com/rodritodev/forum/security/JWTLoginFilter.kt
@@ -0,0 +1,60 @@
+package br.com.rodritodev.forum.security
+
+import br.com.rodritodev.forum.config.JWTUtil
+import br.com.rodritodev.forum.model.Credentials
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+/**
+ * Filtro de autenticação JWT.
+ * Esse filtro é responsável por interceptar as requisições e validar o token JWT presente no cabeçalho da requisição.
+ *
+ * Classe responsavel por tentar autenticar o usuário e gerar o token JWT.
+ *
+ * @property jwtUtil Classe utilitária para manipulação de tokens JWT. *
+ */
+class JWTLoginFilter(private val authManager: AuthenticationManager, private val jwtUtil: JWTUtil) :
+    UsernamePasswordAuthenticationFilter() {
+
+    /**
+     * Método responsável por tentar autenticar o usuário.
+     *
+     * @param request Requisição HTTP.
+     * @param response Resposta HTTP.
+     * @return Objeto de autenticação.
+     */
+    override fun attemptAuthentication(request: HttpServletRequest, response: HttpServletResponse): Authentication {
+        val credentials = ObjectMapper().readValue(request.inputStream, Credentials::class.java)
+        val token = UsernamePasswordAuthenticationToken(credentials.username, credentials.senha)
+        return authManager.authenticate(token)
+    }
+
+    /**
+     * Método responsável por gerar o token JWT e adicionar no cabeçalho da resposta.
+     *
+     * @param request Requisição HTTP.
+     * @param response Resposta HTTP.
+     * @param chain Filtro de requisição.
+     * @param authResult Objeto de autenticação.
+     */
+    override fun successfulAuthentication(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        chain: FilterChain,
+        authResult: Authentication
+    ) {
+        val user = (authResult.principal as UserDetails)
+        val token = jwtUtil.gerarToken(user.username, user.authorities)
+        response.addHeader("Authorization", "Bearer $token")
+        response.addHeader("access-control-expose-headers", "Authorization")
+        response.addHeader("Content-Type", "application/json")
+    }
+
+}
